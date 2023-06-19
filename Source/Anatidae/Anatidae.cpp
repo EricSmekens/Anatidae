d463fa05299c6c343c81b8c456f1e2ec5ba53ae3
Eric Smekens <ericsmekens@msn.com>
2023-06-19 21:42:14 +0200

    Switch to blueprints for now, and updated to 5.2

--- Source/Anatidae/Anatidae.cpp
+++
@@ -1,4 +0,0 @@
-#include "Anatidae.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Anatidae, "Anatidae" );
--- Source/Anatidae/AnatidaeGameModeBase.cpp
+++
@@ -1,2 +0,0 @@
-
-#include "AnatidaeGameModeBase.h"
